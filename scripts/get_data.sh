7fa818c2e7d778f585492f947ae2f53ab9af28ee
danny crasto <danwald79@gmail.com>
2014-10-27 23:03:34 +0400

    made the get_data_script directory agnostic

--- scripts/get_data.sh
+++ scripts/get_data.sh
@@ -1,4 +1,19 @@
 #!/usr/bin/env bash
 
+set -e
+
+SCRIPT_PATH=`dirname $0`
+ROOT_PATH=`( cd $SCRIPT_PATH && cd .. && pwd )`
+DATA_PATH="$ROOT_PATH/data"
+echo $SCRIPT_PATH
+echo $ROOT_PATH
+echo $DATA_PATH
+
+[[ -d $DATA_PATH ]] || mkdir $DATA_PATH
+
+cd $DATA_PATH
 wget http://files.grouplens.org/datasets/movielens/ml-10m.zip
-unzip m1-10m.zip
+unzip ml-10m.zip
+mv ml-10M100K/* .
+rm -rf ml-10M100K
+cd ..

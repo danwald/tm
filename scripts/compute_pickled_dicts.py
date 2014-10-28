2e6f1dcddb6592b4a5dd3b738990b500bc5d0675
danny crasto <danny@dubizzle.com>
2014-10-28 12:59:04 +0400

    gen movies pickle

--- scripts/compute_pickled_dicts.py
+++ scripts/compute_pickled_dicts.py
@@ -3,10 +3,30 @@ if __name__ == '__main__' and __package__ is None:
     sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
 
 import os
+import cPickle as pickle
+import timeit
 from conf.settings import *
 
-def run():
+def gen_movies():
+    print "Generating Movies"
+    movies={}
+    with open(MOVIE_SOURCE) as movie_file:
+        count = 0
+        error_count = 0
+        for movie in movie_file:
+            count += 1
+            try:
+                record = filter(bool, movie.split(':'))
+                movies[int(record[0])] = {'title':record[1],'tags': record[2].split('|')}
+            except:
+                print movie
+                error_count += 1
+                pass
+            print "\rProcessing %d records (errors:%d)"%(count, error_count),
+    pickle.dump(movies, open(MOVIE_PICKLE, 'wb'))
 
+def run():
+    gen_movies()
     return 0
 
 

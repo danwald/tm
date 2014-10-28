1e15d8be77b851c6a5b961199ecf7cc5f47ff88c
danny crasto <danwald79@gmail.com>
2014-10-28 05:47:32 +0400

    wip computed pickled_dicts

--- conf/settings.py
+++ conf/settings.py
@@ -1,6 +1,14 @@
 import os.path
 
 ROOT_PATH=os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+DATA_PATH=os.path.join(ROOT_PATH, "data")
+MODEL_PATH=os.path.join(ROOT_PATH, "models")
+MOVIE_SOURCE=os.path.join(DATA_PATH, "movies.dat")
+MOVIE_PICKLE=os.path.join(DATA_PATH, "movies.pickle")
+TAGS_SOURCE=os.path.join(DATA_PATH, "tags.dat")
+TAGS_PICKLE=os.path.join(DATA_PATH, "tags.pickle")
+RATINGS_SOURCE=os.path.join(DATA_PATH, "ratings.dat")
+RATINGS_PICKLE=os.path.join(DATA_PATH, "ratings.pickle")
 
 PORT=8000
 
--- scripts/compute_pickled_dicts.py
+++ scripts/compute_pickled_dicts.py
@@ -2,5 +2,13 @@ if __name__ == '__main__' and __package__ is None:
     from os import sys, path
     sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
 
-from conf.settings import ROOT_PATH
-print ROOT_PATH
+import os
+from conf.settings import *
+
+def run():
+
+    return 0
+
+
+if __name__ == "__main__":
+    run()

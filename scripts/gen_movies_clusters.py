fd8bf937aa18307a869883a5477a7a91a6344abb
danny crasto <danwald79@gmail.com>
2014-10-31 18:47:17 +0400

    added kmeans for movie recs

--- scripts/gen_movies_clusters.py
+++ scripts/gen_movies_clusters.py
@@ -4,7 +4,8 @@ if __name__ == '__main__' and __package__ is None:
 
 import os
 import cPickle as pickle
-import threading
+import math
+
 from numpy import vstack
 from scipy.cluster.vq import kmeans,vq
 
@@ -21,20 +22,24 @@ def run():
     tags = { value:index for index, value in enumerate(sorted(tags))}
     movie_keys = sorted(movies.keys())
     feature_vectors = []
+    total_tags = 1.0
     for movie_key in movie_keys:
         movie_feature_vector = len(tags)*[0]
         for tag in movies[movie_key]['tags']:
             movie_feature_vector[tags[tag]] = 1
+            total_tags += 1.0
         feature_vectors.append(movie_feature_vector)
+
+    clusters = int(math.ceil((total_tags) / len(movie_keys))) * len(tags)
+    print "Using %d clusters (%d #total_tags / %d #movies) * %d #tags" % (clusters, int(total_tags), len(movie_keys), len(tags))
+
     feature_vectors = vstack(feature_vectors)
-    movies_centroid, min_distorsion = (None, -1)
-    for i in xrange(len(tags)):
-        centroid, distorsion = kmeans(feature_vectors,i+1) 
-        print i, centroid, distorsion
-        if distorsion < min_distorsion:
-            min_distorsion = distorsion
-            movies_centroid = centroid
-    print distorsion, movies_centroid
+    centroids, _ = kmeans(feature_vectors,clusters)
+    clustered_movies, _ = vq(feature_vectors, centroids)
+    pickle.dump(centroids, open(TAGS_PICKLE, 'wb'))
+    pickle.dump(movie_keys, open(TAGS_PICKLE, 'wb'))
+    pickle.dump(clustered_movies, open(TAGS_PICKLE, 'wb'))
+    pickle.dump(tags, open(TAGS_PICKLE, 'wb'))
 
 
 if __name__ == "__main__":
